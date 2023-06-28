77366761ab6ff086ffa0b1de8bbc0a176cf44489
Yarden Katz <yrdnkz@gmail.com>
2023-06-28 12:56:35 +0300

    first check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+	let _stdout = stdout().into_raw_mode().unwrap();
+	
+	for b in io::stdin().bytes() {
+		let c = b.unwrap() as char;
+		println!("{}\r", c);
+		if c == 'q' {
+			break;
+		} 
+	}
 }
