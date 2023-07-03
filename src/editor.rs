42c2e169fcd14942caa932a0d057849fced0c50f
Yarden Katz <yrdnkz@gmail.com>
2023-07-03 11:30:30 +0300

    refactored terminal

--- src/editor.rs
+++ src/editor.rs
@@ -1,50 +1,66 @@
-use std::io::{self, stdout};
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+use crate::Terminal;
 
 pub struct Editor {
 	should_quit: bool,
+	terminal: Terminal
 }
 
 impl Editor {
 	pub fn run(&mut self) {
-		let _stdout = stdout().into_raw_mode().unwrap();
-	
 		loop {
-			if let Err(error) = self.process_keypress() {
+			if let Err(error) = self.refresh_screen() {
 				die(error);
 			}
 
 			if self.should_quit {
 				break;
 			}
+
+			if let Err(error) = self.process_keypress() {
+				die(error);
+			}
 		}
 	}
 
 	pub fn default() -> Self {
-		Self { should_quit: false }
+		Self { 
+			should_quit: false, 
+			terminal: Terminal::default().expect("Failed to initialize terminal"),
+		}
+	}
+
+	fn refresh_screen(&self) -> Result<(), std::io::Error> {
+		Terminal::cursor_hide();
+		Terminal::clear_screen();
+		if self.should_quit {
+			println!("Goodbye!\r");
+		} else {
+			self.draw_rows();
+			Terminal::cursor_position(0, 0);
+		}
+		Terminal::cursor_show();
+		Terminal::flush()
 	}
 
 	fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-		let pressed_key = read_key()?;
+		let pressed_key = Terminal::read_key()?;
 		match pressed_key {
 			Key::Ctrl('x') => self.should_quit = true,
 			_ => (),
 		}
 		Ok(())
 	}
-}
 
-fn read_key() -> Result<Key, std::io::Error> {
-	loop {
-		if let Some(key) = io::stdin().lock().keys().next() {
-			return  key;
+	fn draw_rows(&self) {
+		for _ in 0..self.terminal.size().height - 1 {
+			println!("~\r");
 		}
 	}
 }
 
 fn die(e: std::io::Error) {
+	Terminal::clear_screen();
 	panic!("{}", e);
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, clippy::pedantic)]
+mod terminal;
 mod editor;
 use editor::Editor;
+pub use terminal::Terminal;
 
 fn main() {
 	Editor::default().run();
